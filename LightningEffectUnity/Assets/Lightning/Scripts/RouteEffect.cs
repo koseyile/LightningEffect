8815c3c8483deb74e2247d721e163370a0a3fdae
zhangchenyuan <yuanzhewudi@hotmail.com>
2024-08-01 12:14:51 +0800

    route effect.

--- LightningEffectUnity/Assets/Lightning/Scripts/RouteEffect.cs
+++ LightningEffectUnity/Assets/Lightning/Scripts/RouteEffect.cs
@@ -12,12 +12,15 @@ public class RouteEffect : MonoBehaviour
 {
     public Image RouteImage;
     public Sprite[] RouteSprites;
+    public Sprite[] RouteOriginalSprites;
     public int RouteIndex = 0;
     public RectTransform[] WeldPoints;
     public GameObject RouteCube;
+    public float ShowRoutePathWaitTime = 0.01f;
     private Texture2D mRouteTexture;
     private Color[] mRoutePixels;
     private float mScale = 1f;
+    private bool[] WeldWorkFinished;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,19 +35,21 @@ public class RouteEffect : MonoBehaviour
 
     private void Init()
     {
-        RouteImage.sprite = RouteSprites[RouteIndex];
+        RouteImage.sprite = RouteOriginalSprites[RouteIndex];
         RouteImage.SetNativeSize();
 
-
+        WeldWorkFinished = new bool[WeldPoints.Length];
         for (int i = 0; i < WeldPoints.Length; i++)
         {
             Vector3 p = WeldPoints[i].position;
             WeldPoints[i].anchorMin = Vector2.zero;
             WeldPoints[i].anchorMax = Vector2.zero;
             WeldPoints[i].position = p;
+            WeldWorkFinished[i] = false;
         }
 
-        mRouteTexture = (Texture2D)RouteImage.mainTexture;
+
+        mRouteTexture = RouteSprites[RouteIndex].texture;
         mRoutePixels = mRouteTexture.GetPixels();
         mScale = RouteImage.rectTransform.sizeDelta.x / mRouteTexture.width;
         //Debug.Log("flexibleWidth:" + RouteImage.rectTransform.sizeDelta.x);
@@ -56,33 +61,72 @@ public class RouteEffect : MonoBehaviour
         //List<RoutePoint> routePathBottom = GetRoutePath(Color.red, mRoutePixels, mRouteTexture.width, mRouteTexture.height, false);
         //StartCoroutine(ShowRoutePath(routePathBottom, WeldPoints[1]));
 
-        StartCoroutine(CreateRoutePath(true, WeldPoints[0]));
-        StartCoroutine(CreateRoutePath(false, WeldPoints[1]));
+        StartCoroutine(CreateRoutePath(true, WeldPoints[0], 0, 10));
+        StartCoroutine(CreateRoutePath(false, WeldPoints[1], 1, 10));
     }
 
-    private IEnumerator CreateRoutePath(bool isTop, RectTransform weldPoint)
+    private IEnumerator CreateRoutePath(bool isTop, RectTransform weldPoint, int weldIndex, int showCountPerFrame)
     {
+        weldPoint.gameObject.SetActive(true);
         List<RoutePoint> routePathTop1 = GetRoutePath(Color.red, mRoutePixels, mRouteTexture.width, mRouteTexture.height, isTop);
         routePathTop1 = NearestNeighborSort(routePathTop1);
-        yield return ShowRoutePath(routePathTop1, weldPoint);
+        yield return ShowRoutePath(routePathTop1, weldPoint, showCountPerFrame);
 
         List<RoutePoint> routePathTop2 = GetRoutePath(Color.green, mRoutePixels, mRouteTexture.width, mRouteTexture.height, isTop);
         routePathTop2 = NearestNeighborSort(routePathTop2);
-        yield return ShowRoutePath(routePathTop2, weldPoint);
+        yield return ShowRoutePath(routePathTop2, weldPoint, showCountPerFrame);
 
         List<RoutePoint> routePathTop3 = GetRoutePath(Color.blue, mRoutePixels, mRouteTexture.width, mRouteTexture.height, isTop);
         routePathTop3 = NearestNeighborSort(routePathTop3);
-        yield return ShowRoutePath(routePathTop3, weldPoint);
+        yield return ShowRoutePath(routePathTop3, weldPoint, showCountPerFrame);
+
+        WeldWorkFinished[weldIndex] = true;
+
+        bool isFinished = true;
+        for (int i = 0; i < WeldWorkFinished.Length; i++)
+        {
+            if (WeldWorkFinished[i]==false)
+            {
+                isFinished = false;
+                break;
+            }
+        }
+
+        if ( isFinished )
+        {
+            RouteImage.enabled = isFinished;
+            float a = 0f;
+            while ( a<1f )
+            {
+                a += Time.deltaTime * 2f;
+                Color c = Color.white;
+                c.a = a;
+                RouteImage.color = c;
+                yield return null;
+            }
+        }
+
+        
     }
 
-    private IEnumerator ShowRoutePath(List<RoutePoint> routePath, RectTransform weldPoint)
+    private IEnumerator ShowRoutePath(List<RoutePoint> routePath, RectTransform weldPoint, int showCountPerFrame)
     {
-        foreach (var item in routePath)
+        //foreach (var item in routePath)
+        for (int i = 0; i < routePath.Count; i+=showCountPerFrame)
         {
-            weldPoint.anchoredPosition = item.position;
-            GameObject go = GameObject.Instantiate(RouteCube);
-            go.transform.position = weldPoint.position;
-            yield return new WaitForSeconds(0.05f);
+            for (int j = 0; j < showCountPerFrame; j++)
+            {
+                int index = i + j;
+
+                if ( index<routePath.Count )
+                {
+                    weldPoint.anchoredPosition = routePath[index].position;
+                    GameObject go = GameObject.Instantiate(RouteCube);
+                    go.transform.position = weldPoint.position;
+                }
+            }
+
+            yield return new WaitForSeconds(ShowRoutePathWaitTime);
         }
     }
 
